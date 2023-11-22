288ad383c9c8941071c0be718ebbf7a1428ea9bf
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-22 23:43:26 +0000

    Deploying to gh-pages from @ Escbrk/goit-js-hw-08@7b05ac67744c1dbb35cc32dce930fde2314041a2 🚀

--- 03-feedback.d408d328.js
+++
@@ -1,2 +0,0 @@
-console.log(function(n,o){const e={};for(let n=1;n<=o;n+=1)e[n]=0;return n.forEach((n=>{const o=Object.values(e),t=Math.min(...o),c=value.indexOf(t)+1;e[c]+=n})),e}([12,3,5,6,2,15,7],2));
-//# sourceMappingURL=03-feedback.d408d328.js.map
