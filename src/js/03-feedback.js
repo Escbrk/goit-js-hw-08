3942a8870cb9f09fb1f97c7370555805ab9777d8
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-25 12:25:02 -0700

    finished hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,25 +5,46 @@ const refs = {
   input: document.querySelector("[type='email']"),
   textArea: document.querySelector("[name='message']"),
   submitBtn: document.querySelector('[type="submit"]'),
+  FEEDBACK_FORM_STATE: 'feedback-form-state',
 };
-const FEEDBACK_FORM_STATE = 'feedback-form-state';
-const FEEDBACK_VALUE = JSON.stringify(refs.textArea.value.textContent);
 
-const objData = {};
-
-refs.input.addEventListener('input', onInput);
+refs.input.addEventListener('input', throttle(onInput, 500));
+refs.textArea.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', onSubmit);
 
 function onInput(e) {
-  e.preventDefault();
+  const data = {
+    email: refs.form.email.value,
+    message: refs.form.message.value,
+  };
 
-  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(FEEDBACK_VALUE));
+  localStorage.setItem(refs.FEEDBACK_FORM_STATE, JSON.stringify(data));
 }
 
-console.log(FEEDBACK_VALUE);
-console.log(refs.input);
-console.log(refs.textArea);
-console.log(refs.submitBtn);
+function updateForm() {
+  const data = JSON.parse(localStorage.getItem(refs.FEEDBACK_FORM_STATE));
+  console.log(data);
+  
+  refs.input.value = data.email
+  refs.textArea.value = data.message
+}
 
-localStorage.setItem(FEEDBACK_FORM_STATE, FEEDBACK_VALUE);
+function onSubmit(e) {
+  e.preventDefault();
 
-console.log(localStorage.getItem(FEEDBACK_FORM_STATE));
+  if (refs.input.value === '' || refs.textArea.value === '') {
+     return alert('Все поля должны быть заполнены!');
+  }
+
+  const data = {
+    email: refs.form.email.value,
+    message: refs.form.message.value,
+  };
+
+  console.log(data);
+
+  refs.form.reset();
+  localStorage.removeItem(refs.FEEDBACK_FORM_STATE)
+}
+
+updateForm();
